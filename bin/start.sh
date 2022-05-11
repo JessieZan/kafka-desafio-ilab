e3a176e90290923d034003184f7ec3db18601f50
Julio Neto <nettojulio@hotmail.com>
2022-05-11 12:03:49 -0300

    clean project

--- bin/start.sh
+++
@@ -1,9 +0,0 @@
-export KAFKA_HOST="localhost:9092"
-export KAFKA_TOPIC="topico_grupo3"
-export KAFKA_GROUP_ID_READER="consumer_3"
-
-export CLASSPATH=target/kafka_consumer-1.0-SNAPSHOT.jar
-export className=ServerApplication
-echo "## Running $className..."
-shift
-mvn exec:java -Dexec.mainClass="com.kafka.server;.$className"
